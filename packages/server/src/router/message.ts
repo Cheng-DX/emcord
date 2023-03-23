9fd10fb5b6632512a6fc5f5b15788fc17438a712
Cheng-DX <2337040896@qq.com>
2023-03-23 10:22:41 +0800

    chore: temparily disable messages routes

--- packages/server/src/router/message.ts
+++ packages/server/src/router/message.ts
@@ -1,41 +1,41 @@
-import type { Router } from 'express'
-import { useMessageModel } from '../db/models/message'
+// import type { Router } from 'express'
+// import { useMessageModel } from '../db/models/message'
 
-export function applyMessage(router: Router) {
-  router.post('/message', async (req, res) => {
-    const content = req.body
-    const { channelId } = content
+// export function applyMessage(router: Router) {
+//   router.post('/message', async (req, res) => {
+//     const content = req.body
+//     const { channelId } = content
 
-    const model = useMessageModel(`MESSAGE-${channelId}`)
-    try {
-      const result = await model.create(content)
-      res.status(200).json(result)
-    }
-    catch (e) {
-      res.status(488).json({
-        message: `[message]: Database add error: ${e}`,
-      })
-    }
-  })
+//     const model = useMessageModel(`MESSAGE-${channelId}`)
+//     try {
+//       const result = await model.create(content)
+//       res.status(200).json(result)
+//     }
+//     catch (e) {
+//       res.status(488).json({
+//         message: `[message]: Database add error: ${e}`,
+//       })
+//     }
+//   })
 
-  router.get('/message/:channelId/:size', async (req, res) => {
-    const { size, channelId } = req.params
+//   router.get('/message/:channelId/:size', async (req, res) => {
+//     const { size, channelId } = req.params
 
-    const model = useMessageModel(`MESSAGE-${channelId}`)
+//     const model = useMessageModel(`MESSAGE-${channelId}`)
 
-    try {
-      const results = await model
-        .find()
-        .limit(Number(size))
-        .sort({
-          time: 'desc',
-        })
-      res.status(200).json(results)
-    }
-    catch (e) {
-      res.status(488).json({
-        message: `[message]: Database add error: ${e}`,
-      })
-    }
-  })
-}
+//     try {
+//       const results = await model
+//         .find()
+//         .limit(Number(size))
+//         .sort({
+//           time: 'desc',
+//         })
+//       res.status(200).json(results)
+//     }
+//     catch (e) {
+//       res.status(488).json({
+//         message: `[message]: Database add error: ${e}`,
+//       })
+//     }
+//   })
+// }
